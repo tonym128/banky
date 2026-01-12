6882e4d17d61208d5457c6bf8c11f50f3473d0c0
Tony Mamacos <tmamacos@gmail.com>
2026-01-12 15:48:32 +1100

    pwa

--- sw.js
+++ sw.js
@@ -1,13 +1,18 @@
-const CACHE_NAME = 'kids-bank-cache-v2';
+const CACHE_NAME = 'kids-bank-cache-v3';
 const urlsToCache = [
     '/',
     'index.html',
     'styles.css',
     'app.js',
+    'ui.js',
+    'pwa.js',
+    'state.js',
     'idb.js',
     'manifest.json',
     'images/banky.png',
     'images/logo.svg',
+    'android-chrome-192x192.png',
+    'android-chrome-512x512.png',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
     'https://cdn.jsdelivr.net/npm/chart.js'
